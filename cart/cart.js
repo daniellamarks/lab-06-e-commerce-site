625280b43802d203a291a022e3a5f940a58c4619
Daniella Marks <marks.daniella@gmail.com>
2021-03-31 15:17:55 -0700

    generate shopping cart

--- cart/cart.js
+++ cart/cart.js
@@ -7,29 +7,29 @@ console.log(denimStyles);
 //grab the table from the dom
 const table = document.querySelector('.item-table');
 //we loop through our cart (id, quantity)
+
 for (let cartItem of cart) {
-    //for every cart item, render a tr element
-    // how do i use the cartItem to find a match denimStyle?
-        //need a findbyid function to loop 
-    //we are going to find the correct digimon from the array of all digimon WITH the id of the cart that matches that digmon    
 
-    //we need matchingdenim to get the name and price,
-    //we need the cartItem to get the quantity
-
-    //for each cart item, finds its matching denim style
-    const matchingDenimStyle = findById(denimStyles, cartItem.id);
-
-    console.log(matchingDenimStyle, cartItem);
-
+        //start with cart and find all product matches. 
+    const productMatch = findById(denimStyles, cartItem.id);
+    //now i have the denim match, i need to 
+    const tr = createTableRow(cartItem, productMatch);
     
-    //use the denim style and the cart to make some html
-    const tr = createTableRow(cartItem, matchingDenimStyle);
-    //and append that tr to the table
-
-  
     table.append(tr);
 }
 
-const totalRow = createTotalRow(cart, denimStyles);
 
-table.append(totalRow);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ notes.js
@@ -0,0 +1,26 @@
+for (let cartItem of cart) {
+    //for every cart item, render a tr element
+    // how do i use the cartItem to find a match denimStyle?
+        //need a findbyid function to loop 
+    //we are going to find the correct digimon from the array of all digimon WITH the id of the cart that matches that digmon    
+
+    //we need matchingdenim to get the name and price,
+    //we need the cartItem to get the quantity
+
+    //for each cart item, finds its matching denim style
+    const matchingDenimStyle = findById(denimStyles, cartItem.id);
+
+    console.log(matchingDenimStyle, cartItem);
+
+    
+    //use the denim style and the cart to make some html
+    const tr = createTableRow(cartItem, matchingDenimStyle);
+    //and append that tr to the table
+
+  
+    table.append(tr);
+}
+
+const totalRow = createTotalRow(cart, denimStyles);
+
+table.append(totalRow);
